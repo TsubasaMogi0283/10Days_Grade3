634da03bc86d2f19339e84437bad1351bc4229cc
翼 茂木 <k022g0130@m.neec.ac.jp>
2024-09-08 15:53:53 +0900

    追跡処理。 敵同士の当たり判定凄いことになったから余裕があるときに直す。

--- Project/Enemy/EnemyManager.cpp
+++ Project/Enemy/EnemyManager.cpp
@@ -47,8 +47,8 @@ void EnemyManager::Tracking(){
 	//接近するときの距離
 	const float TRACKING_START_DISTANCE_ = 40.0f;
 	////攻撃するときの距離
-	const float ATTACK_START_DISTANCE_ = 6.0f;
-	const float MINIMUM_DISTANCE = 2.0f;
+	//const float ATTACK_START_DISTANCE_ = 6.0f;
+	//const float MINIMUM_DISTANCE = 2.0f;
 
 
 
@@ -64,7 +64,7 @@ void EnemyManager::Tracking(){
 	AABB playerAABB = { .min = playerMinAABB , .max = playerMaxAABB };
 
 	//1体だけの時
-	if (enemyQuantity == 1u) {
+	if (enemyQuantity >= 1u) {
 		for (Enemy* enemy : enemyes_) {
 
 			//AABBを取得
@@ -131,109 +131,121 @@ void EnemyManager::Tracking(){
 
 
 
-
-
-	//1体より多い時
-	if (enemyQuantity > 1u) {
-		for (std::list<Enemy*>::iterator it1 = enemyes_.begin(); it1 != enemyes_.end(); ++it1) {
-
-			//AABB
-			AABB enemy1AABB = (*it1)->GetAABB();
-			AABB enemy2AABB = {};
-			//位置1
-			Vector3 enemy1Position = (*it1)->GetWorldPosition();
-			//向き
-			Vector3 direction1 = (*it1)->GetDirection();
-
-			float dot = 0.0f;
-
-			for (std::list<Enemy*>::iterator it2 = enemyes_.begin(); it2 != enemyes_.end(); ++it2) {
-
-				//it1とit2が一致した場合は計算をせずに次のループへ
-				if (it1 == it2) {
-					continue;
-				}
-
-				//2体目のAABBを取得
-				enemy2AABB = (*it2)->GetAABB();
-
-				//接触している場合
-				if ((enemy1AABB.min.x < enemy2AABB.max.x && enemy1AABB.max.x > enemy2AABB.min.x) &&
-					(enemy1AABB.min.y < enemy2AABB.max.y && enemy1AABB.max.y > enemy2AABB.min.y) &&
-					(enemy1AABB.min.z < enemy2AABB.max.z && enemy1AABB.max.z > enemy2AABB.min.z)) {
-					//ワールド座標
-					Vector3 enemy2Position = (*it2)->GetWorldPosition();
-
-
-					//敵同士の差分ベクトル
-					Vector3 enemyAndEnemyDifference = VectorCalculation::Subtract(enemy1Position, enemy2Position);
-
-					//進行方向の前にいると+
-					Vector3 normalizedEnemyAndEnemy = VectorCalculation::Normalize(enemyAndEnemyDifference);
-					//内積
-					dot = SingleCalculation::Dot(direction1, normalizedEnemyAndEnemy);
-
-
-					break;
-				}
-
-			}
-
-
-
-
-			//プレイヤーとの距離
-			Vector3 playerEnemyDifference = VectorCalculation::Subtract(playerPosition_, (*it1)->GetWorldPosition());
-			float playerEnemyDistance = SingleCalculation::Length(playerEnemyDifference);
-
-
-			//敵1の現在の状態
-			uint32_t condition = (*it1)->GetCondition();
-
-
-			//前方にいた場合
-			if (dot > 0.9f && condition == EnemyCondition::Move) {
-
-
-
-
-				//接触した場合
-				//止まる
-				if ((enemy1AABB.min.x < enemy2AABB.max.x && enemy1AABB.max.x > enemy2AABB.min.x) &&
-					(enemy1AABB.min.y < enemy2AABB.max.y && enemy1AABB.max.y > enemy2AABB.min.y) &&
-					(enemy1AABB.min.z < enemy2AABB.max.z && enemy1AABB.max.z > enemy2AABB.min.z)) {
-					uint32_t newCondition = EnemyCondition::NoneMove;
-
-					//前の状態を保存
-					(*it1)->SetPreCondition(condition);
-					//状態の変更
-					(*it1)->SetCondition(newCondition);
-
-					continue;
-				}
-				//接触していない場合
-				else {
-					uint32_t newCondition = EnemyCondition::Move;
-
-					//前の状態を保存
-					(*it1)->SetPreCondition(condition);
-					//状態の変更
-					(*it1)->SetCondition(newCondition);
-				}
-			}
-
-
-			//前方にいない場合
-			else {
-			}
-
-
-
-
-
-
-		}
-	}
+	////1体より多い時
+	//if (enemyQuantity > 1u) {
+	//	for (std::list<Enemy*>::iterator it1 = enemyes_.begin(); it1 != enemyes_.end(); ++it1) {
+	//
+	//		//AABB
+	//		AABB enemy1AABB = (*it1)->GetAABB();
+	//		AABB enemy2AABB = {};
+	//		//位置1
+	//		Vector3 enemy1Position = (*it1)->GetWorldPosition();
+	//		//向き
+	//		Vector3 direction1 = (*it1)->GetDirection();
+	//
+	//		//float dot = 0.0f;
+	//
+	//		////2体目
+	//		//for (std::list<Enemy*>::iterator it2 = enemyes_.begin(); it2 != enemyes_.end(); ++it2) {
+	//		//
+	//		//	//it1とit2が一致した場合は計算をせずに次のループへ
+	//		//	if (it1 == it2) {
+	//		//		continue;
+	//		//	}
+	//		//
+	//		//	//2体目のAABBを取得
+	//		//	enemy2AABB = (*it2)->GetAABB();
+	//		//
+	//		//	//接触している場合
+	//		//	if ((enemy1AABB.min.x < enemy2AABB.max.x && enemy1AABB.max.x > enemy2AABB.min.x) &&
+	//		//		(enemy1AABB.min.y < enemy2AABB.max.y && enemy1AABB.max.y > enemy2AABB.min.y) &&
+	//		//		(enemy1AABB.min.z < enemy2AABB.max.z && enemy1AABB.max.z > enemy2AABB.min.z)) {
+	//		//		//ワールド座標
+	//		//		Vector3 enemy2Position = (*it2)->GetWorldPosition();
+	//		//
+	//		//
+	//		//		//敵同士の差分ベクトル
+	//		//		Vector3 enemyAndEnemyDifference = VectorCalculation::Subtract(enemy1Position, enemy2Position);
+	//		//
+	//		//		//進行方向の前にいると+
+	//		//		Vector3 normalizedEnemyAndEnemy = VectorCalculation::Normalize(enemyAndEnemyDifference);
+	//		//		//内積
+	//		//		dot = SingleCalculation::Dot(direction1, normalizedEnemyAndEnemy);
+	//		//
+	//		//
+	//		//		break;
+	//		//	}
+	//		//
+	//		//}
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//
+	//			//AABBを取得
+	//			AABB enemyAABB = enemy->GetAABB();
+	//
+	//			//状態
+	//			uint32_t condition = enemy->GetCondition();
+	//
+	//			//向き
+	//			Vector3 enemyDirection = enemy->GetDirection();
+	//
+	//			//プレイヤーと敵の差分ベクトル
+	//			//対象にするものがプレイヤーなのでプレイヤーから引いてね
+	//			Vector3 defference = VectorCalculation::Subtract(playerPosition_, enemy->GetWorldPosition());
+	//
+	//
+	//
+	//			Vector3 normalizedDefference = VectorCalculation::Normalize(defference);
+	//			float dot = SingleCalculation::Dot(normalizedDefference, enemyDirection);
+	//
+	//
+	//
+	//
+	//			//距離を求める
+	//			float distance = SingleCalculation::Length(defference);
+	//
+	//			//前方にいる時だけ追跡する
+	//			if (dot > 0.8f) {
+	//
+	//
+	//				if (distance < TRACKING_START_DISTANCE_ && condition == EnemyCondition::Move) {
+	//
+	//					//前の状態を保存
+	//					enemy->SetPreCondition(condition);
+	//
+	//					//現在の状態を保存
+	//					uint32_t newCondition = EnemyCondition::PreTracking;
+	//					enemy->SetPlayerPosition(playerPosition_);
+	//					enemy->SetCondition(newCondition);
+	//				}
+	//
+	//			}
+	//			else {
+	//				//前の状態を保存
+	//				enemy->SetPreCondition(condition);
+	//				//現在の状態を保存
+	//				uint32_t newCondition = EnemyCondition::Move;
+	//				enemy->SetCondition(newCondition);
+	//			}
+	//
+	//
+	//
+	//
+	//	}
+	//}
 }
 
 
